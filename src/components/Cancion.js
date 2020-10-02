055eb7329f6020e655d4629c9c6967684308277d
Victor Sanchez <victor1305@icloud.com>
2020-10-02 15:17:30 +0200

    [STABLE][v1.0]

---
+++ src/components/Cancion.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+const Cancion = ({letra}) => {
+
+    if(!letra) return null
+
+    return (
+
+        <Fragment>
+            <h2>Letra Canción</h2>
+            <p className = "letra">{letra}</p>
+        </Fragment>
+
+    )
+}
+ 
+export default Cancion;
